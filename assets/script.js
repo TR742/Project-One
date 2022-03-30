ba071be72daac2ceefaa3053d9de0698c8bf5ddf
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-03-29 18:48:29 -0700

    added package-lock.json

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,57 @@
 response = document.getElementById('response')
 
 function getApi(){
-let url = "https://dog.ceo/api/breeds/image/random/13";
+  fetch("https://dog.ceo/api/breeds/image/random/13")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(err => alert(err))
+}
 
 
-fetch(url)
-.then(function (response) {
-  return response.json();
-})
+
+// let url="https://dog.ceo/api/breeds/image/random/13"
+
+// fetch(url)
+// .then(function (response) {
+//   return response.json();
+// })
+// .then((data) => alert("HEY TAYLOR", response.json(data)))
 
 
-.then(function (data) {
   // console.log(data)
-  for (let i = 0; i < data.length; i++){
+// fetch(url)
+// .then(function (response) {
+//   return response.json();
+// })
+// .then(data => {
+//   console.log(data.message);
+//   displayImg(data)
+// })
 
- console.log(data[i])
+// console.log(data.message)
+
+// .then(function (data) {
+//   // console.log(data)
+//   for (let i = 0; i < data.length; i++){
+
+//   }
+// })}
+// getApi()
 
 
-  let i1 = document.createElement('img');
-      i1.textContent = data[i].message;
-  //  console.log(data[i].message)
 
-   response.append(i1);  
-      }
-    });
+//   let i1 = document.createElement('img');
+//       i1.textContent = data[i].message;
+//   //  console.log(data[i].message)
 
- }
+//    response.append(i1);  
+//       }
+//     });
 
-console.log(getApi)
- getApi()
+//  }
+
+// console.log(getApi)
+//  getApi()
 
 
 
@@ -43,9 +66,3 @@ console.log(getApi)
   //     throw new Error("NETWORK RESPONSE ERROR");
   //   }
   // })
-  // .then(data => {
-  //   console.log(data.message);
-  //   displayImg(data)
-  // })
-
-  // console.log(data.message);
