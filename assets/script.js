0cdd10cc4a5b600435e0d5fa3112ef4dfbfe76ee
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-03-30 11:25:39 -0700

    called and looped through API array of dog imgs

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,97 @@
-response = document.getElementById('response')
-
-function getApi(){
-  fetch("https://dog.ceo/api/breeds/image/random/13")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => alert(err))
-}
+//  url ="https://dog.ceo/api/breeds/image/random/13";
 
 
+fetch("https://dog.ceo/api/breeds/image/random/13")
+  .then(function(response){
+    return response.json()})
 
-// let url="https://dog.ceo/api/breeds/image/random/13"
+  .then( function(data) {
+        
+    console.log(data)
+    console.log(data.message)
+    for(let i=0; i < data.message.length; i++){
 
-// fetch(url)
+      let images = document.querySelector('#response')
+      console.log(images)
+    // console.log(data)
+    let imgList = document.createElement('img');
+    imgList.src = data.message[i];
+    images.appendChild(imgList);
+    }
+  })
+
+  
+
+ 
+
+
+
+
+
+
+
+
+
+// function getApi(){
+// response = document.querySelector('response')
+// requestUrl = 'https://dog.ceo/api/breeds/image/random/13'
+
+//  fetch(requestUrl)
 // .then(function (response) {
 //   return response.json();
+//   console.log(response)
 // })
-// .then((data) => alert("HEY TAYLOR", response.json(data)))
+// }
 
 
-  // console.log(data)
-// fetch(url)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(data => {
-//   console.log(data.message);
-//   displayImg(data)
-// })
+// // let url="https://dog.ceo/api/breeds/image/random/13"
 
-// console.log(data.message)
+// // fetch(url)
+// // .then(function (response) {
+// //   return response.json();
+// // })
+// // .then((data) => alert("HEY TAYLOR", response.json(data)))
 
-// .then(function (data) {
+
 //   // console.log(data)
-//   for (let i = 0; i < data.length; i++){
 
-//   }
-// })}
-// getApi()
+// // })
+
+// // console.log(data.message)
+
+// // .then(function (data) {
+// //   // console.log(data)
+// //   for (let i = 0; i < data.length; i++){
+
+// //   }
+// // })}
+// //  getApi()
 
 
 
-//   let i1 = document.createElement('img');
-//       i1.textContent = data[i].message;
-//   //  console.log(data[i].message)
+// //   let i1 = document.createElement('img');
+// //       i1.textContent = data[i].message;
+// //   //  console.log(data[i].message)
 
-//    response.append(i1);  
-//       }
-//     });
+// //    response.append(i1);  
+// //       }
+// //     });
 
-//  }
+// //  }
 
-// console.log(getApi)
-//  getApi()
+// // console.log(getApi)
+// //  getApi()
 
 
 
  
 
 
- // fetch(url)
-  // .then((response) => {
-  //   if (response.ok) {
-  //     return response.json();
-  //   } else {
-  //     throw new Error("NETWORK RESPONSE ERROR");
-  //   }
-  // })
+//  // fetch(url)
+//   // .then((response) => {
+//   //   if (response.ok) {
+//   //     return response.json();
+//   //   } else {
+//   //     throw new Error("NETWORK RESPONSE ERROR");
+//   //   }
+//   // })
