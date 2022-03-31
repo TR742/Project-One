99d38809d5dee9aaf924cc4c6be8b3baa216feb1
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-03-30 18:48:29 -0700

    icons and font

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+// Fetching API and responding the request in JSON 
 fetch("https://dog.ceo/api/breeds/image/random/2")
   .then(function(response){
     return response.json()})
@@ -7,8 +7,11 @@ fetch("https://dog.ceo/api/breeds/image/random/2")
         
     console.log(data)
     console.log(data.message)
+
     for(let i=0; i < data.message.length; i++){
 
+
+// Dynamically creating a carousel element with images are iterated and appended to
       let images = document.querySelector("#carouselExampleSlidesOnly");
       console.log(images)
     // console.log(data)
